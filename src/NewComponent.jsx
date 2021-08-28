b5eea6410fc014deea5b0d19c8ec3cae823ca6b7
Edwin10000 <eamoreno898@misena.edu.co>
2021-08-27 20:31:23 -0500

    Clase del 27 de agosto de componente y llenando datos desde array

---
+++ src/NewComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewComponent = () => (
+    <div className="contenedor">
+        <h3>Titulo nuevo componente</h3>
+        <p>Texto de prueba</p>
+    </div>
+)
+export default NewComponent;
